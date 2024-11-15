0d2088cb3e0eb9a5f140be7953b0174d2109be43
shrikantg199 <shrikantg199@gmail.com>
2024-11-15 18:53:55 +0530

    initial commit

--- app/(drawer)/(tabs)/Post.jsx
+++ app/(drawer)/(tabs)/Post.jsx
@@ -1,16 +1,21 @@
 import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
   StyleSheet,
   Text,
-  View,
-  TouchableOpacity,
   TextInput,
-  Button,
-  Image,
+  View,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation, useRouter } from "expo-router";
-import { Entypo } from "@expo/vector-icons";
+import { Entypo, FontAwesome5 } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker";
+import { TouchableOpacity } from "react-native";
+import { db, storage } from "./../../../firebaseConfig";
+import { getDownloadURL, uploadBytes, ref } from "firebase/storage";
+import uuid from "react-native-uuid";
+import { addDoc, collection } from "firebase/firestore";
 const Post = () => {
   const navigation = useNavigation();
   const router = useRouter();
@@ -25,24 +30,69 @@ const Post = () => {
       quality: 1,
     });
 
-    console.log(result);
+    // console.log(result?.assets[0]?.uri);
 
     if (!result.canceled) {
       setImage(result.assets[0].uri);
+      uploadImage(result.assets[0].uri);
     }
   };
+  // upload function
+  const uploadImage = async (uri) => {
+    // fetch
+    const response = await fetch(uri);
+    // blob
+    const blob = await response.blob();
+    // unique id package
+    const uniqueName = uuid.v4();
+    //
+    const storageRef = ref(storage, `image${uniqueName}`);
+    await uploadBytes(storageRef, blob);
+    const downloadUrl = await getDownloadURL(storageRef);
+    console.log(downloadUrl);
+    return downloadUrl;
+  };
+
+  // add url in database
+  const postHandler = async () => {
+    // console.log("clicked");
+    let imageUrl = null;
+    if (image) {
+      imageUrl = await uploadImage(image);
+    }
+    try {
+      await addDoc(collection(db, "posts"), {
+        imageUrl: imageUrl,
+      });
+      console.log("Post added to Firestore successfully.");
+      alert("Post created successfully!");
+
+      setImage(null);
+      router.back(); // Navigate back after successful post
+    } catch (error) {
+      console.error("Error adding post to Firestore: ", error);
+      alert("Failed to create post.");
+    }
+  };
+
   useEffect(() => {
     navigation.setOptions({
       headerTitle: "Create Post",
       headerLeft: () => (
         // icon
-        <TouchableOpacity onPress={() => router.back()}>
-          <Entypo name="cross" size={32} color="black" />
-        </TouchableOpacity>
+        <Entypo
+          onPress={() => {
+            console.log("Back button pressed");
+            router.back();
+          }}
+          name="cross"
+          size={32}
+          color="black"
+        />
       ),
       headerRight: () => (
-        <TouchableOpacity className="mx-2">
-          <Text className="bg-blue-600 px-2 py-2 rounded-xl text-white">
+        <TouchableOpacity className="mx-2" onPress={postHandler}>
+          <Text className="bg-blue-600 px-3 py-2 rounded-xl text-white">
             Post
           </Text>
         </TouchableOpacity>
@@ -50,21 +100,30 @@ const Post = () => {
     });
   });
   return (
-    <View className="">
+    <KeyboardAvoidingView
+      className="h-screen"
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
       <View>
         <TextInput
-          placeholder="Enter Post content"
+          placeholder="Enter post content"
           className="text-2xl"
           multiline
           autoFocus={true}
         />
+        {image && (
+          <Image source={{ uri: image }} className="h-96 w-full mt-10" />
+        )}
       </View>
-      {/* image */}
-      <Button title="image picker" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} className="h-44 w-full" />}
-    </View>
+      <TouchableOpacity
+        onPress={pickImage}
+        className="absolute bottom-36 right-10 bg-blue-600 px-3 py-3 rounded-full "
+      >
+        {/* image icon */}
+        <FontAwesome5 name="photo-video" size={20} color="white" />
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
   );
 };
-export default Post;
 
-const styles = StyleSheet.create({});
+export default Post;
